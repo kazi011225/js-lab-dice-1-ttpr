4b35b008de188232c0ea450cc46f0fb0c940fddd
Hessvacio Hassan <madeinbrooklyngames@gmail.com>
2025-04-30 00:41:25 -0400

    removes solution - DONT CHEAT

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let dice = {
   sides: 6,
   roll: function () {
-  let randomNumber = Math.floor(Math.random() * this.sides) + 1;
+  let randomNumber;
   // Write Code Here
   // Generate a random number between 1 and the number of sides and assign it to randomNumber
 
